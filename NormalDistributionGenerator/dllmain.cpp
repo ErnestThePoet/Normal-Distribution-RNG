a1307e89054bd1fc0ce62b8d7e6d2d51f557e896
Ernest Cui <ecuiships@126.com>
2021-11-20 15:39:23 +0800

    Migrated to full-LCG generation for performance, excluding initialization

--- NormalDistributionGenerator/dllmain.cpp
+++ NormalDistributionGenerator/dllmain.cpp
@@ -6,61 +6,76 @@
 #include <cmath>
 #include <ctime>
 #include <cstdint>
-#include <random>
 #include <memory>
+#include <array>
+#include <random>
 
-#define TWO_PI 6.28318530717958647692f
-#define GENERATE_FLOAT \
-	sqrtf(-2 * sigma_square_ * logf(uniform_distribution_(engine_))) \
-	* cosf(TWO_PI* uniform_distribution_(engine_)) + mu_
+#define TWO_PI 6.28318530717958647692f	
 
-#define M256_CONST_TYPE_ALIGN32(Name,Type,Val) \
+#define M256_32BIT_CONST_TYPE_ALIGNED(Name,Type,Val) \
 	static constexpr __declspec(align(32)) Type Name[8]={Val,Val,Val,Val,Val,Val,Val,Val}
 
 using std::mt19937;
-using std::uniform_real_distribution;
+using std::array;
 using std::unique_ptr;
 using std::make_unique;
 
 // 正态分布随机数生成器类
-class NormalDistributionGenerator
+class NormalDistributionRNG
 {
 private:
 	float mu_ = 0.0f;
 	float sigma_square_ = 1.0f;
 
-	mt19937 engine_;
-	uniform_real_distribution<float> uniform_distribution_;
-
 	float* generated_floats_ = nullptr;
 
-	M256_CONST_TYPE_ALIGN32(lcg_uint32_a_, uint32_t, 1664525);
-	M256_CONST_TYPE_ALIGN32(lcg_uint32_b_, uint32_t, 1013904223);
+	static constexpr size_t vector_size_ = 8;
 
-	M256_CONST_TYPE_ALIGN32(packed_uint32_exp0_mask_, uint32_t, 0x3F800000);
+	static constexpr uint32_t lcg32_a_ = 1664525;
+	static constexpr uint32_t lcg32_b_ = 1013904223;
+	static constexpr uint32_t float_exp0_mask_ = 0x3F800000;
 
-	M256_CONST_TYPE_ALIGN32(packed_float_1_, float, 1.0f);
-	M256_CONST_TYPE_ALIGN32(packed_float_2pi_, float, TWO_PI);
+	M256_32BIT_CONST_TYPE_ALIGNED(packed_lcg32_a_, uint32_t, lcg32_a_);
+	M256_32BIT_CONST_TYPE_ALIGNED(packed_lcg32_b_, uint32_t, lcg32_b_);
 
-	__m256i previous_lcg_i32s_;
+	M256_32BIT_CONST_TYPE_ALIGNED(packed_float_exp0_mask_, uint32_t, float_exp0_mask_);
+
+	M256_32BIT_CONST_TYPE_ALIGNED(packed_1f_, float, 1.0f);
+	M256_32BIT_CONST_TYPE_ALIGNED(packed_2pi_, float, TWO_PI);
+
+	uint32_t previous_lcg_ui32_;
+
+	float GetNextLCGFloat_()
+	{
+		previous_lcg_ui32_ = previous_lcg_ui32_ * lcg32_a_ + lcg32_b_;
+		union
+		{
+			uint32_t ui;
+			float f;
+		} u;
+		u.ui = (previous_lcg_ui32_ >> 9) | float_exp0_mask_;
+		return u.f - 1.0f;
+	}
+
+	__m256i packed_previous_lcg_ui32_;
 
 	__m256 GetNextLCGPack_()
 	{
 		// 对整数向量完成一次线性同余迭代
-		previous_lcg_i32s_ = _mm256_add_epi32(
-			_mm256_mullo_epi32(previous_lcg_i32s_, *(__m256i*)lcg_uint32_a_),
-			*(__m256i*)lcg_uint32_b_);
+		packed_previous_lcg_ui32_ = _mm256_add_epi32(
+			_mm256_mullo_epi32(packed_previous_lcg_ui32_, *(__m256i*)packed_lcg32_a_),
+			*(__m256i*)packed_lcg32_b_);
 		
 		// 将整数向量的位模式转换为能产生[1.0,2.0)之间的浮点数位模式
 		// 首先右移清空符号位和指数位，然后使用掩码将指数位置127
 		__m256i float_bit_ready_i32s = _mm256_or_epi32(
-			_mm256_srli_epi32(previous_lcg_i32s_, 9),
-			*(__m256i*) packed_uint32_exp0_mask_);
+			_mm256_srli_epi32(packed_previous_lcg_ui32_, 9),
+			*(__m256i*) packed_float_exp0_mask_);
 
 		// 转换为float向量，减去1.0并返回
 		__m256 result = _mm256_sub_ps(
 			_mm256_castsi256_ps(float_bit_ready_i32s), 
-			*(__m256*) packed_float_1_);
+			*(__m256*) packed_1f_);
 
 		return result;
 	}
@@ -71,25 +86,29 @@ public:
 	* 参数mu: 正态分布的期望
 	* 参数sigma_square: 正态分布的方差
 	*************************************************/
-	NormalDistributionGenerator(
+	NormalDistributionRNG(
 		float mu,
 		float sigma_square) :
 		mu_(mu),
-		sigma_square_(sigma_square),
-		engine_(time(nullptr)),
-		uniform_distribution_(0, 1),
-		previous_lcg_i32s_(_mm256_set_epi32(
-			engine_(),
-			engine_(),
-			engine_(), 
-			engine_(), 
-			engine_(), 
-			engine_(), 
-			engine_(),
-			engine_()))
-	{}
+		sigma_square_(sigma_square)
+	{
+		mt19937 initial_ui32_generator(time(nullptr));
 
-	~NormalDistributionGenerator()
+		previous_lcg_ui32_ = initial_ui32_generator();
+
+		packed_previous_lcg_ui32_ = _mm256_set_epi32(
+			initial_ui32_generator(),
+			initial_ui32_generator(),
+			initial_ui32_generator(),
+			initial_ui32_generator(),
+			initial_ui32_generator(),
+			initial_ui32_generator(),
+			initial_ui32_generator(),
+			initial_ui32_generator()
+		);
+	}
+
+	~NormalDistributionRNG()
 	{
 		if (generated_floats_)
 		{
@@ -120,12 +139,13 @@ public:
 	float* FloatsAVX2(unsigned int count);
 };
 
-float NormalDistributionGenerator::NextFloat()
+float NormalDistributionRNG::NextFloat()
 {
-	return GENERATE_FLOAT;
+	return sqrtf(-2 * sigma_square_ * logf(GetNextLCGFloat_()))
+		* cosf(TWO_PI * GetNextLCGFloat_()) + mu_;
 }
 
-float* NormalDistributionGenerator::Floats(unsigned int count)
+float* NormalDistributionRNG::Floats(unsigned int count)
 {
 	if (generated_floats_)
 	{
@@ -136,13 +156,15 @@ float* NormalDistributionGenerator::Floats(unsigned int count)
 
 	for (unsigned int i = 0; i < count; i++)
 	{
-		generated_floats_[i] = GENERATE_FLOAT;
+		generated_floats_[i] = 
+			sqrtf(-2 * sigma_square_ * logf(GetNextLCGFloat_()))
+			* cosf(TWO_PI * GetNextLCGFloat_()) + mu_;
 	}
 
 	return generated_floats_;
 }
 
-float* NormalDistributionGenerator::FloatsAVX2(unsigned int count)
+float* NormalDistributionRNG::FloatsAVX2(unsigned int count)
 {
 	if (generated_floats_)
 	{
@@ -154,7 +176,7 @@ float* NormalDistributionGenerator::FloatsAVX2(unsigned int count)
 	__m256 packed_m2_sigma_square = _mm256_set1_ps(-2 * sigma_square_);
 	__m256 packed_mu = _mm256_set1_ps(mu_);
 
-	for (unsigned int i = 0; i < count / 8; i++)
+	for (unsigned int i = 0; i < count / vector_size_; i++)
 	{
 		__m256 result = _mm256_sqrt_ps(_mm256_mul_ps(
 			_mm256_log_ps(GetNextLCGPack_()),
@@ -162,11 +184,11 @@ float* NormalDistributionGenerator::FloatsAVX2(unsigned int count)
 
 		result = _mm256_mul_ps(
 			_mm256_cos_ps(_mm256_mul_ps(
-				*(__m256*)packed_float_2pi_,
+				*(__m256*)packed_2pi_,
 				GetNextLCGPack_())),
 			result);
 
-		_mm256_store_ps(generated_floats_ + 8 * i,
+		_mm256_store_ps(generated_floats_ + vector_size_ * i,
 			_mm256_add_ps(result, packed_mu));
 	}
 
@@ -176,16 +198,19 @@ float* NormalDistributionGenerator::FloatsAVX2(unsigned int count)
 
 	remaining_result = _mm256_mul_ps(
 		_mm256_cos_ps(_mm256_mul_ps(
-			*(__m256*)packed_float_2pi_,
+			*(__m256*)packed_2pi_,
 			GetNextLCGPack_())),
 		remaining_result);
 
-	float remaining_results_array[8] = { 0 };
-	_mm256_store_ps(remaining_results_array, remaining_result);
+	array<float, vector_size_> remaining_results_array{ 0 };
+	_mm256_store_ps(remaining_results_array.data(), remaining_result);
 
-	for (unsigned int i = 8 * (count / 8); i < count; i++)
+	for (unsigned int i = vector_size_ * (count / vector_size_);
+		i < count;
+		i++)
 	{
-		generated_floats_[i] = remaining_results_array[i - 8 * (count / 8)];
+		generated_floats_[i] = 
+			remaining_results_array[i - vector_size_ * (count / vector_size_)];
 	}
 
 	return generated_floats_;
@@ -207,34 +232,33 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 	return TRUE;
 }
 
-unique_ptr<NormalDistributionGenerator> normal_distribution_generator = nullptr;
+unique_ptr<NormalDistributionRNG> kRNG = nullptr;
 
 // 以下均为Dll导出函数
 void ExportCreateGenerator(
 	float mu,
 	float sigma_square)
 {
-	normal_distribution_generator =
-		make_unique<NormalDistributionGenerator>(mu, sigma_square);
+	kRNG = make_unique<NormalDistributionRNG>(mu, sigma_square);
 }
 
 float ExportNextFloat()
 {
-	if (normal_distribution_generator)
+	if (kRNG)
 	{
-		return normal_distribution_generator->NextFloat();
+		return kRNG->NextFloat();
 	}
 	else
 	{
-		return 0.0;
+		return 0.0f;
 	}
 }
 
 float* ExportFloats(unsigned int count)
 {
-	if (normal_distribution_generator)
+	if (kRNG)
 	{
-		return normal_distribution_generator->Floats(count);
+		return kRNG->Floats(count);
 	}
 	else
 	{
@@ -244,9 +268,9 @@ float* ExportFloats(unsigned int count)
 
 float* ExportFloatsAVX2(unsigned int count)
 {
-	if (normal_distribution_generator)
+	if (kRNG)
 	{
-		return normal_distribution_generator->FloatsAVX2(count);
+		return kRNG->FloatsAVX2(count);
 	}
 	else
 	{
